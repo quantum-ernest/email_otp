677434e98d1542a62608465f695444789efe3d14
quantum-ernest <easare@quantumgroupgh.com>
2024-08-14 18:46:11 +0000

    update middleware

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI, responses
+from starlette.middleware.cors import CORSMiddleware
+
 from controllers.users import router as users_router
 from controllers.auth import router as auth_router
 
@@ -6,7 +8,13 @@ app = FastAPI(title="IAM with OTP via Email")
 app.include_router(users_router, prefix="/api/users", tags=["USERS"])
 app.include_router(auth_router, prefix="/api/auth/otp", tags=["AUTH"])
 
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 @app.get("/", include_in_schema=False)
 def root():
     return responses.RedirectResponse(url="/docs")
