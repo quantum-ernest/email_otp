807801137d12f9c6bf56fd59e25ce3c68800a373
quantum-ernest <easare@quantumgroupgh.com>
2024-08-23 13:31:47 +0000

    update database import

--- alembic/env.py
+++ alembic/env.py
@@ -6,12 +6,12 @@ from sqlalchemy import pool
 from alembic import context
 
 from models import users
-from config.env import envConfig
+from config.database import DATABASE_URL
 from config.database import Base
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-config.set_main_option("sqlalchemy.url", envConfig.DATABASE_URL)
+config.set_main_option("sqlalchemy.url", DATABASE_URL)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
