27bbdd72e15b836b53ecec0a5a6347585cf360c3
quantum-ernest <easare@quantumgroupgh.com>
2024-08-23 13:42:57 +0000

    add doc string

--- utils/random_otp.py
+++ utils/random_otp.py
@@ -2,4 +2,12 @@ import random
 
 
 def generate_otp() -> str:
+    """
+    Generate a 6-digit One-Time Password (OTP).
+
+    This function generates a random 6-digit OTP using Python's random module.
+
+    Returns:
+        str: A 6-digit OTP as a string.
+    """
     return str(random.randint(100000, 999999))
