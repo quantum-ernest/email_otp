b7a7ca5c732de08b17f2126f58893c6b8bab61b8
quantum-ernest <easare@quantumgroupgh.com>
2024-08-23 22:41:11 +0000

    change port

--- api_starter.sh
+++ api_starter.sh
@@ -3,4 +3,4 @@
 alembic upgrade head
 
 
-uvicorn main:app --reload --host 0.0.0.0 --port 8001 --forwarded-allow-ips="*"
+uvicorn main:app --reload --host 0.0.0.0 --port 8000 --forwarded-allow-ips="*"
