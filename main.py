f7ead84d2681dae95bf025c33c1b985adf637298
quantum-ernest <easare@quantumgroupgh.com>
2024-08-23 13:45:16 +0000

    add doc string

--- main.py
+++ main.py
@@ -4,9 +4,12 @@ from starlette.middleware.cors import CORSMiddleware
 from controllers.users import router as users_router
 from controllers.auth import router as auth_router
 
-app = FastAPI(title="IAM with OTP via Email")
-app.include_router(users_router, prefix="/api/users", tags=["USERS"])
+from config.database import Base, engine
+
+Base.metadata.create_all(bind=engine)
+app = FastAPI(title="OTP Authentication via Email")
 app.include_router(auth_router, prefix="/api/auth/otp", tags=["AUTH"])
+app.include_router(users_router, prefix="/api/users", tags=["USERS"])
 
 app.add_middleware(
     CORSMiddleware,
@@ -15,6 +18,14 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+
 @app.get("/", include_in_schema=False)
 def root():
+    """
+    Redirect the root URL to the API documentation.
+
+    Returns:
+        RedirectResponse: Redirects to the "/docs" URL.
+    """
     return responses.RedirectResponse(url="/docs")
